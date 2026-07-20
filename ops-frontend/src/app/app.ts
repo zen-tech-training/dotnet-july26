b66d7d4b87e201bbd58f0afdf3c3df5a4e41df49
rupeshbhoir-zen <rupesh.bhoir@zensar.com>
2026-07-20 15:05:51 +0530

    Angular v1

--- ops-frontend/src/app/app.ts
+++ ops-frontend/src/app/app.ts
@@ -4,10 +4,12 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { GetDataFromAPI } from './component/get-data-from-api/get-data-from-api';
 import { Navbar } from './shared/components/navbar/navbar';
+import { UserList } from './features/user-list/user-list';
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, GetDataFromAPI, Navbar],
+  imports: [RouterOutlet, GetDataFromAPI, Navbar, UserList],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
---
+++ ops-frontend/src/app/features/user-list/user-list.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-user-list',
+  imports: [],
+  templateUrl: './user-list.html',
+  styleUrl: './user-list.scss',
+})
+export class UserList {}
