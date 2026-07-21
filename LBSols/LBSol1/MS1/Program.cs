b685d6adc75a9a0c5044844ffa829968c3df3935
rupeshbhoir-zen <rupesh.bhoir@zensar.com>
2026-07-21 12:08:23 +0530

    Load Balancer and API Gateway

---
+++ LBSols/LBSol1/MS1/Program.cs
@@ -0,0 +1,47 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddOpenApi();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+}
+
+app.UseHttpsRedirection();
+
+app.MapGet("/", () => "Hello world! from MS1"); //http://localhost:5001/
+app.MapGet("/lb", () => {
+    var instanceId = Environment.GetEnvironmentVariable("ASPNETCORE_URLS") ?? "Unknown Instance";
+    return Results.Content($"<h1> Hello World! From MS1  {instanceId}</h1>", "text /html");
+});
+
+var summaries = new[]
+{
+    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+};
+
+app.MapGet("/weatherforecast", () =>
+{
+    var forecast =  Enumerable.Range(1, 5).Select(index =>
+        new WeatherForecast
+        (
+            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+            Random.Shared.Next(-20, 55),
+            summaries[Random.Shared.Next(summaries.Length)]
+        ))
+        .ToArray();
+    return forecast;
+})
+.WithName("GetWeatherForecast");
+
+app.Run(); //https://localhost:5001/openapi/v1.json
+
+record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
+{
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+}
---
+++ LBSols/LBSol3/ApiGateway/Program.cs
@@ -0,0 +1,12 @@
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddReverseProxy()
+    .LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"));
+
+var app = builder.Build();
+
+app.MapReverseProxy();
+
+app.MapGet("/", () => "Hello World!"); //Minimal API
+
+app.Run();
