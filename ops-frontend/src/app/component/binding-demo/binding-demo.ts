0a45c74bbd98a720e678e59cae01d2564033d167
rupeshbhoir-zen <rupesh.bhoir@zensar.com>
2026-07-23 14:46:39 +0530

    binding demo v2

--- ops-frontend/src/app/component/binding-demo/binding-demo.ts
+++ ops-frontend/src/app/component/binding-demo/binding-demo.ts
@@ -1,9 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-binding-demo',
   standalone:true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './binding-demo.html',
   styleUrl: './binding-demo.scss',
 })
@@ -13,5 +14,28 @@ export class BindingDemo {
   isDisabled = false;  //Property Binding
 
   imgHeight = "50%"; //Units - em, rem, cm, %, px
+
+  quantity = 0;
+  
+  incrementCounter(){
+    this.quantity = this.quantity + 1;
+
+    this.secondCounter.update(cur => cur + 1);
+
+    console.log(this.quantity);
+  }
+  decrementCounter(){
+    if(this.quantity>1){
+      this.quantity = this.quantity - 1;
+      this.secondCounter.update(cur => cur - 1);
+    }
+  }
+
+  msgPost = "ppppppppppppppp";
+
+  secondCounter = signal(0);
+
+
+
 
 }
